1d6c67b3d03e8e387024cbd132bdb17c11f55d8f
YannLeao <yann.kjleao@ufrpe.br>
2025-04-30 02:15:07 -0300

    feat: Praticando Pandas

---
+++ database/1873-calculate_special_bonus.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = employees.apply(
+        lambda x: x['salary'] if (x['name'][0] != 'M' and x['employee_id'] % 2 != 0) else 0,
+        axis=1
+    )
+    return employees[['employee_id', 'bonus']].sort_values(by='employee_id')
